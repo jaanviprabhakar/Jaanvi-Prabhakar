2123ff6669c7542d1c9e7bd7645c55490a48225a
Jaanvi Prabhakar <jaanviprabhakar@gmail.com>
2021-01-13 07:26:03 +0530

    Updates and Comments

--- main.js
+++ main.js
@@ -3,5 +3,7 @@ $(document).ready(function(){
 	$('.menu-toggler').on('click',function(){
 		$(this).toggleClass('open');
 		$('.top-nav').toggleClass('open');
+
+		// toggleClass works as a switch
 	});
 });
